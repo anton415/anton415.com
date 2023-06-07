1b8bca9ba5c242afd7ebe91d423d08dae5f23939
Anton Serdyuchenko <anton415@gmail.com>
2023-06-07 10:16:00 +0300

    Updating website.
    - Removed test content.
    - Added link to story.
    - Moved Menu to separated component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
-import CameraIcon from '@mui/icons-material/PhotoCamera';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -10,28 +8,24 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Footer from './Footer';
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+import Menu from './Menu';
 
 const theme = createTheme();
+const story = {
+  name: 'Horny little kitty',
+  url: 'https://literotica.com/s/horny-little-kitty'
+};
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar position="relative">
-        <Toolbar>
-          <CameraIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Menu
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Menu />
       <main>
         {/* Hero unit */}
         <Box
@@ -62,39 +56,28 @@ export default function App() {
             </Stack>
           </Container>
         </Box>
-        <Container sx={{ py: 8 }} maxWidth="md">
-          {/* End hero unit */}
+        {/* End hero unit */}
+        <Container maxWidth="md">
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    sx={{
-                      // 16:9
-                      pt: '56.25%',
-                    }}
-                    image="https://source.unsplash.com/random"
-                    alt="random"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the
-                      content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small">View</Button>
-                    <Button size="small">Edit</Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
+            <Grid>
+              <Card>
+                <CardMedia />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {story.name}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Link
+                    href={story.url}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    View
+                  </Link>
+                </CardActions>
+              </Card>
+            </Grid>
           </Grid>
         </Container>
       </main>
