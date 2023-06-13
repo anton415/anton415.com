b3a8207fa1739495d78e74bd462335d15fb7405f
Anton Serdyuchenko <anton415@gmail.com>
2023-06-13 11:01:42 +0300

    Updates

---
+++ precache-manifest.67897e84bbd2dd62ef5cf043844ffeb4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf5c15b4d24717c89b13",
+    "url": "/static/js/main.bf5c15b4.chunk.js"
+  },
+  {
+    "revision": "6521d45265532d9f5d7f",
+    "url": "/static/js/1.6521d452.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "13cb2e1bf43d85b141ce9e9c9ba86c0d",
+    "url": "/index.html"
+  }
+];
