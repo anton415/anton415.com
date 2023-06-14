40a4c8214f3f4dfe27d0f64495feee7b6df9975b
Anton Serdyuchenko <anton415@gmail.com>
2023-06-14 10:10:14 +0300

    Updating.
    - Removed unused components.
    - Added story img.
    - Added links.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,31 @@ import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
 import CssBaseline from '@mui/material/CssBaseline';
-import Grid from '@mui/material/Grid';
-import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
+import Divider from '@mui/material/Divider';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Footer from './Footer';
-import Menu from './Menu';
+import img from './static/images/horny-little-kitty.webp';
 
 const theme = createTheme();
 const story = {
   name: 'Horny little kitty',
-  url: 'https://literotica.com/s/horny-little-kitty'
+  url: 'https://literotica.com/s/horny-little-kitty',
+  description: 'A young girl went to a Halloween party in a catsuit where she will show her wild instincts and desire to obey.'
 };
+
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Menu />
       <main>
-        {/* Hero unit */}
+        {/* Main unit */}
         <Box
           sx={{
             bgcolor: 'background.paper',
@@ -44,40 +45,42 @@ export default function App() {
             >
               Anton415 portfolio
             </Typography>
-            <Stack
-              sx={{ pt: 4 }}
-              direction="row"
-              spacing={2}
-              justifyContent="center"
-            >
-              <Button variant="contained">Erotic content</Button>
-              <Button variant="outlined">Non-erotic content</Button>
-            </Stack>
+            <Divider />
+              <Link href="https://www.literotica.com/stories/memberpage.php?uid=6695609&page=submissions">literotica</Link>
+              {' '}
+              <Link href="https://medium.com/@anton415">medium</Link>
+              {' '}
+              <Link href="https://twitter.com/anton415226">twitter</Link>
           </Container>
         </Box>
-        {/* End hero unit */}
-        <Container maxWidth="md">
-          <Grid container spacing={4}>
-            <Grid>
-              <Card>
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {story.name}
-                  </Typography>
-                </CardContent>
-                <CardActions>
-                  <Link
-                    href={story.url}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    View
-                  </Link>
-                </CardActions>
-              </Card>
-            </Grid>
-          </Grid>
-        </Container>
+        {/* End main unit */}
+
+<Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="194"
+        image={img}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {story.name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {story.description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button
+          href={story.url}
+          rel="noopener noreferrer"
+          target="_blank"
+          size="small"
+        >
+          Literotica
+        </Button>
+      </CardActions>
+    </Card>
+
       </main>
       <Footer />
     </ThemeProvider>
