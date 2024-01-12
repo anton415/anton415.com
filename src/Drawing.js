5c88985ea11efdf7cb3f2fda6150ae394e271bcc
Anton Serdyuchenko <anton415@gmail.com>
2024-01-12 09:46:38 +0300

    Moved to semantic ui. Optimized for mobile view.

--- src/Drawing.js
+++ src/Drawing.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import Box from '@mui/material/Box';
+import { CardGroup, Card } from 'semantic-ui-react'
 import FirstImg from './static/drawings/1.jpeg';
 import SecondImg from './static/drawings/2.jpeg';
 import ThirdImg from './static/drawings/3.jpeg';
@@ -12,52 +10,20 @@ import SeventhImg from './static/drawings/7.jpeg';
 import EighthImg from './static/drawings/8.jpeg';
 import NinthImg from './static/drawings/9.jpeg';
 
-export default function Drawing() {
-  return (
-      <Box align="center" >
-         <ImageList sx={{ width: 500 }} cols={3} >
-            {itemData.map((item) => (
-              <ImageListItem key={item.img}>
-                <img
-                  srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                  src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                  alt={item.title}
-                  loading="lazy"
-                />
-              </ImageListItem>
-            ))}
-          </ImageList>
-      </Box>
+const Drawing = () => (
+  <CardGroup centered items={items} />
+)
 
-  );
-}
+export default Drawing
 
-const itemData = [
-  {
-    img: FirstImg
-  },
-  {
-    img: SecondImg
-  },
-  {
-    img: ThirdImg
-  },
-  {
-    img: FourthImg
-  },
-  {
-    img: FifthImg
-  },
-  {
-    img: SixthImg
-  },
-  {
-    img: SeventhImg
-  },
-  {
-    img: EighthImg
-  },
-  {
-    img: NinthImg
-  }
+const items = [
+  {image: FirstImg},
+  {image: SecondImg},
+  {image: ThirdImg},
+  {image: FourthImg},
+  {image: FifthImg},
+  {image: SixthImg},
+  {image: SeventhImg},
+  {image: EighthImg},
+  {image: NinthImg}
 ];
