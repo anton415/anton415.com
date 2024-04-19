346ad07b47be130902de163a0a757e960d2ad416
Anton Serdyuchenko <anton415@gmail.com>
2024-04-19 10:16:23 +0300

    Clean up.
    - Removing unused files.
    - Moving top bar in different file.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,8 @@ import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import TopBar from './TopBar';
 import theme from './theme';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import Box from '@mui/material/Box';
 import AntonSerdyuchenkoMobileAvatar from './static/images/avatar/AntonSerdyuchenko-Mobile.webp';
 
 const rootElement = document.getElementById('root');
@@ -19,17 +15,7 @@ root.render(
     <ThemeProvider theme={theme}>
       {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
       <CssBaseline />
-      <AppBar position="static">
-        <Toolbar>
-          <Box >
-            <Avatar alt="Anton Serdyuchenko" src={AntonSerdyuchenkoMobileAvatar} />
-          </Box>
-          <Box sx={{ flexGrow: 1 }} />
-          <Typography alignItems="center" sx={{ flexGrow: 1 }}>
-            Art of Anton Serdyuchenko
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <TopBar />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
