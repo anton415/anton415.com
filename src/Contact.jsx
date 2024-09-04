508507a38027bc64b66cac4f5d79776d0205c161
Anton Serdyuchenko <anton415@gmail.com>
2024-09-04 20:15:29 +0300

    - Updating links to social media profiles.

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import PinterestIcon from '@mui/icons-material/Pinterest';
-import EmailIcon from '@mui/icons-material/Email';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
@@ -14,45 +11,46 @@ import WorkIcon from '@mui/icons-material/Work';
 import BeachAccessIcon from '@mui/icons-material/BeachAccess';
 import Divider from '@mui/material/Divider';
 import ListItemButton from '@mui/material/ListItemButton';
-import CaraIcon from './static/icons/cara-icon.png';
 import PixivIcon from './static/icons/pixiv-logo.png';
 import TwitterIcon from './static/icons/twitter-logo.png';
+import MailIcon from './static/icons/mail-logo.png';
 
 export default function Contact() {
   return (
     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <ListItem disablePadding>
-        <ListItemButton component="a" href="https://cara.app/anton415/">
+        <ListItemButton component="a" href="https://www.pixiv.net/en/users/108453050/">
           <ListItemAvatar>
             <Avatar
               alt="Pixiv"
               src={PixivIcon}
             />
           </ListItemAvatar>
-          <ListItemText primary="Pixiv" secondary="My portfolio." />
+          <ListItemText primary="Pixiv" secondary="My works." />
         </ListItemButton>
       </ListItem>
       <Divider />
       <ListItem disablePadding>
-        <ListItemButton component="a" href="https://cara.app/anton415/">
+        <ListItemButton component="a" href="https://x.com/anton415226/">
           <ListItemAvatar>
             <Avatar
               alt="Twitter"
               src={TwitterIcon}
             />
           </ListItemAvatar>
-          <ListItemText primary="Twitter" secondary="My portfolio." />
+          <ListItemText primary="Twitter" secondary="News." />
         </ListItemButton>
       </ListItem>
       <Divider />
       <ListItem disablePadding>
         <ListItemButton component="a" href="mailto:anton415@gmail.com">
           <ListItemAvatar>
-            <Avatar>
-              <EmailIcon />
-            </Avatar>
+            <Avatar
+              alt="Mail"
+              src={MailIcon}
+            />
           </ListItemAvatar>
-          <ListItemText primary="Email" secondary="Reach out with any questions!" />
+          <ListItemText primary="Mail" secondary="Reach out with any questions!" />
         </ListItemButton>
       </ListItem>
     </List>
